f07734a9cac53353bce8f92d51408db637df5958
pseudoureka <messiah7179@gmail.com>
2024-11-12 21:40:40 +0900

    리스폰스 다루기

--- pages/api/short-links/index.js
+++ pages/api/short-links/index.js
@@ -1,3 +1,28 @@
 export default function handler(req, res) {
-  res.send("안녕 API!");
+  switch (req.method) {
+    case "POST":
+      res.status(201).send({
+        title: "네이버",
+        url: "https://www.naver.com",
+      });
+      break;
+
+    case "GET":
+      res.send([
+        {
+          id: "1",
+          title: "다음",
+          url: "https://www.daum.net",
+        },
+        {
+          id: "2",
+          title: "구글",
+          url: "https://www.google.com",
+        },
+      ]);
+      break;
+
+    default:
+      res.status(404).send();
+  }
 }
