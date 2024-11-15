08191c3a3da447c2bbab06d0a84928d2ed720bf0
pseudoureka <messiah7179@gmail.com>
2024-11-15 22:45:09 +0900

    short-link patch API 연동

--- pages/short-links/[id].js
+++ pages/short-links/[id].js
@@ -1,8 +1,38 @@
-import Head from 'next/head';
-import ShortLinkForm, { ShortLinkFormType } from '@/components/ShortLinkForm';
-import styles from '@/styles/ShortLinkEditPage.module.css';
+import Head from "next/head";
+import ShortLinkForm, { ShortLinkFormType } from "@/components/ShortLinkForm";
+import styles from "@/styles/ShortLinkEditPage.module.css";
+import axios from "@/lib/axios";
+import dbConnect from "@/db/dbConnect";
+import ShortLink from "@/db/models/ShortLink";
+import { notFound } from "next/navigation";
+import { useRouter } from "next/router";
 
-export default function ShortLinkEditPage() {
+export async function getServerSideProps(context) {
+  await dbConnect();
+
+  const { id } = context.query;
+  const shortLink = await ShortLink.findById(id);
+  if (shortLink) {
+    return {
+      props: {
+        shortLink: JSON.parse(JSON.stringify(shortLink)),
+      },
+    };
+  }
+  return {
+    notFound: true,
+  };
+}
+
+export default function ShortLinkEditPage({ shortLink }) {
+  const router = useRouter();
+  const { id } = router.query;
+
+  const handleSubmit = async (values) => {
+    axios.patch(`/short-links/${id}`, values);
+    router.push("/short-links");
+  };
+
   return (
     <>
       <Head>
@@ -10,7 +40,11 @@ export default function ShortLinkEditPage() {
       </Head>
       <div className={styles.page}>
         <h1 className={styles.title}>수정하기</h1>
-        <ShortLinkForm type={ShortLinkFormType.Edit} />
+        <ShortLinkForm
+          onSubmit={handleSubmit}
+          initialValues={shortLink}
+          type={ShortLinkFormType.Edit}
+        />
       </div>
     </>
   );
